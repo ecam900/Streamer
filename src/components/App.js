fe9ed23b319a488c1d8b611791810eb1c55acb61
Eric Crescioni <ecam900@gmail.com>
2019-04-04 18:37:01 -0400

    Example code with react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+//Example code to delete later
+
+const PageOne = () => {
+  return <div>PageOne</div>
+};
+const PageTwo = () => {
+  return (
+     <div>
+      PageTwo
+      <button>Click me!</button>
+    </div>
+  )
+};
+
+//End Example Code
+
 const App = () => {
-  return <div>App</div>
+  return(
+    <div>
+      <BrowserRouter>
+        <div>
+        <Route path="/" exact  component={PageOne} />
+        <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  )
 };
 
 export default App;
