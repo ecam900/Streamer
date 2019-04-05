b4b86727f4bea2e0d5c1b9c966cd1cf8096e26b1
Eric Crescioni <ecam9@RTIAUPC0001.rti.local>
2019-04-05 19:47:57 -0400

    Scaffold components

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-//Example code to delete later
-
-const PageOne = () => {
-  return <div>PageOne</div>
-};
-const PageTwo = () => {
-  return (
-     <div>
-      PageTwo
-      <button>Click me!</button>
-    </div>
-  )
-};
-
-//End Example Code
-
 const App = () => {
   return(
     <div>
       <BrowserRouter>
         <div>
-        <Route path="/" exact  component={PageOne} />
-        <Route path="/pagetwo" component={PageTwo} />
+          
         </div>
       </BrowserRouter>
     </div>
