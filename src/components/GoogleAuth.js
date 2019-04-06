2de9e5697c712facaef845013230f010832a8134
Eric Crescioni <ecam9@RTIAUPC0001.rti.local>
2019-04-05 20:36:12 -0400

    Mod header. Add GoogleAuth

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+
+  componentDidMount() {
+    window.gapi.load('client:auth2');
+  }
+
+  render() {
+    return <div>GoogleAuth</div>
+  }
+}
+
+export default GoogleAuth;
