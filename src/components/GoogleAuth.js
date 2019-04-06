dda9acdf672ca5cab37773990f0f819b704971e9
Eric Crescioni <ecam9@RTIAUPC0001.rti.local>
2019-04-05 20:40:20 -0400

    Add OAUTH. Add lifecycle method to GoogleAuth

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -3,7 +3,12 @@ import React from 'react';
 class GoogleAuth extends React.Component {
 
   componentDidMount() {
-    window.gapi.load('client:auth2');
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: '1054552990644-m6bcd28q02orco4d6mqjcjd0mdj53ble.apps.googleusercontent.com',
+        scope: 'email'
+      });
+    });
   }
 
   render() {
